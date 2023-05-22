a4cd57df6ed2843ea535b7ad766cfabe15181e2f
Kimberley Johnson <kimjaydot@gmail.com>
2023-05-22 13:32:50 -0500

    added github repo url

--- src/App.js
+++ src/App.js
@@ -1,6 +1,19 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project was coded by Kim Johnson
+        <a
+          href="https://github.com/kimjaydot/my-react-weather-app"
+          target="blank"
+        >
+          {" "}
+          Open-sourced on Github.com
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
