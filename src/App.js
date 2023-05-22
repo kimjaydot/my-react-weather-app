cb08254c69c92b7893d504cb6863cc3a8247e257
Kimberley Johnson <kimjaydot@gmail.com>
2023-05-22 15:13:57 -0500

    styled inside content, applied css

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
-      <footer>
-        This project was coded by Kim Johnson
-        <a
-          href="https://github.com/kimjaydot/my-react-weather-app"
-          target="blank"
-        >
-          {" "}
-          Open-sourced on Github.com
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <Weather />
+
+        <footer>
+          This project was coded by Kim Johnson
+          <a
+            href="https://github.com/kimjaydot/my-react-weather-app"
+            target="blank"
+          >
+            {" "}
+            Open-sourced on Github.com
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
